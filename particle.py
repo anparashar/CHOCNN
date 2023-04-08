ede32fbb0ee9cca7093ebb2d8f5e95c4298e587a
anparashar <42533950+anparashar@users.noreply.github.com>
2023-04-08 13:49:24 +0530

    Update particle.py

--- particle.py
+++ particle.py
@@ -2,15 +2,15 @@ import numpy as np
 from copy import deepcopy
 import keras.backend
 import tensorflow as tf
-from keras.models import Model, Sequential
-from keras.layers import Input, Add, Dense, Dropout, Flatten
-from keras.layers import Activation, Conv2D, MaxPooling2D, AveragePooling2D
-from keras.layers.advanced_activations import LeakyReLU
-from keras.layers.advanced_activations import ReLU
-from keras import regularizers 
-from keras.optimizers import Adam, Nadam
-from keras.preprocessing.image import ImageDataGenerator
-from keras.layers import BatchNormalization
+from tf.keras.models import Model, Sequential
+from tf.keras.layers import Input, Add, Dense, Dropout, Flatten
+from tf.keras.layers import Activation, Conv2D, MaxPooling2D, AveragePooling2D
+from tf.keras.layers.advanced_activations import LeakyReLU
+from tf.keras.layers.advanced_activations import ReLU
+from tf.keras import regularizers 
+from tf.keras.optimizers import Adam, Nadam
+from tf.keras.preprocessing.image import ImageDataGenerator
+from tf.keras.layers import BatchNormalization
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2' 
 
