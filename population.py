bac8edf10b818549e80b143d60f01e71a661cd86
anparashar <42533950+anparashar@users.noreply.github.com>
2023-04-08 13:16:32 +0530

    Update population.py

--- population.py
+++ population.py
@@ -1,4 +1,4 @@
-from particle import Particle
+from CHOCNN.particle import Particle
 
 class Population:
     def __init__(self, pop_size, input_width, input_height, input_channels, max_conv_kernel, max_out_ch, max_pool_kernel, max_fc_neurons, output_dim):
